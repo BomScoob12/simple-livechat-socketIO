a027e1798a4a7031d4970d0f748054d663706c6d
BomScoob12 <sorawitkraukham@gmail.com>
2024-07-01 14:53:54 +0700

    refactor: remove unused components

--- simple-livechat-fe/src/views/ChatRoom.vue
+++ simple-livechat-fe/src/views/ChatRoom.vue
@@ -1,12 +1,23 @@
 <script setup>
-import { io } from "socket.io-client";
+import { io } from 'socket.io-client'
 
-const socket = io('http://localhost:3000');
+const socket = io('http://localhost:3000')
 
+const sendMessage = (event) => {
+  console.log('send message')
+  const msg = event.target[0].value
+  console.log(msg)
+  socket.emit('send-message', msg)
+}
 </script>
 
 <template>
   <div>
+    <div class="chat-box"></div>
+    <form @submit.prevent="sendMessage($event)">
+      <input type="text" />
+      <button>Send</button>
+    </form>
   </div>
 </template>
 
